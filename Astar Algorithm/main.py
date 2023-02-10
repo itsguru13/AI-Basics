cefe9d8dd1a71ab38f847782c3d088588371598b
Gurucharan Shivakumar <gurushivkrish@gmail.com>
2023-02-10 09:52:16 +0530

    update files

--- Astar Algorithm/main.py
+++ Astar Algorithm/main.py
@@ -1,121 +1,27 @@
-from copy import deepcopy
-import numpy as np
-import time
+import heapq
 
-def bestsolution(state):
-    bestsol = np.array([], int).reshape(-1, 9)
-    count = len(state) - 1
-    while count != -1:
-        bestsol = np.insert(bestsol, 0, state[count]['puzzle'], 0)
-        count = (state[count]['parent'])
-    return bestsol.reshape(-1, 3, 3)
+def a_star(start, goal, graph):
+    heap = [(0, start)]
+    v = set()
+    p = {}
+    g = {start: 0}
+    while heap:
+        n = heapq.heappop(heap)
+        if n == goal:
+            return n + [p[n] for n in p][::-1]
+        if n in v:
+            continue
+        v.add(n)
+        for i in graph.neighbors(n):
+            cost = g[n] + graph.cost(n, i)
+            if i not in g or cost < g[i]:
+                g[i] = cost
+                heapq.heappush(heap, (cost + heuristic_cost_estimate(i, goal), i))
+                p[i] = n
+    return None
 
-       
-def all(checkarray):
-    set=[]
-    for it in set:
-        for checkarray in it:
-            return 1
-        else:
-            return 0
-
-
-def misplaced_tiles(puzzle,goal):
-    mscost = np.sum(puzzle != goal) - 1
-    return mscost if mscost > 0 else 0
-
-
-def coordinates(puzzle):
-    pos = np.array(range(9))
-    for p, q in enumerate(puzzle):
-        pos[q] = p
-    return pos
+def heuristic_cost_estimate(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
-def evaluvate_misplaced(puzzle, goal):
-    steps = np.array([('up', [0, 1, 2], -3),('down', [6, 7, 8],  3),('left', [0, 3, 6], -1),('right', [2, 5, 8],  1)],
-                dtype =  [('move',  str, 1),('position', list),('head', int)])
-
-    dtstate = [('puzzle',  list),('parent', int),('gn',  int),('hn',  int)]
-
-    costg = coordinates(goal)
-    parent = -1
-    gn = 0
-    hn = misplaced_tiles(coordinates(puzzle), costg)
-    state = np.array([(puzzle, parent, gn, hn)], dtstate)
-
-    dtpriority = [('position', int),('fn', int)]
-
-    priority = np.array([(0, hn)], dtpriority)
-    
-    while 1:
-        priority = np.sort(priority, kind='mergesort', order=['fn', 'position'])      
-        position, fn = priority[0]       
-        priority = np.delete(priority, 0, 0)                         
-        puzzle, parent, gn, hn = state[position]
-        puzzle = np.array(puzzle)
-         
-        blank = int(np.where(puzzle == 0)[0])   
-      
-        gn = gn + 1                             
-        c = 1
-        start_time = time.time()
-        for s in steps:
-            c = c + 1
-            if blank not in s['position']:
-                openstates = deepcopy(puzzle)         
-                openstates[blank], openstates[blank + s['head']] = openstates[blank + s['head']], openstates[blank]
-               
-                if ~(np.all(list(state['puzzle']) == openstates, 1)).any():          
-                    end_time = time.time()
-                    if (( end_time - start_time ) > 2):
-                        print(" The 8 puzzle is unsolvable \n")
-                        break
-                    
-                    hn = misplaced_tiles(coordinates(openstates), costg) 
-                    q = np.array([(openstates, position, gn, hn)], dtstate)         
-                    state = np.append(state, q, 0)
-                    fn = gn + hn                                        
-                    
-                    q = np.array([(len(state) - 1, fn)], dtpriority)
-                    priority = np.append(priority, q, 0)
-                    
-                    if np.array_equal(openstates, goal):                      
-                        print(' The 8 puzzle is solvable \n')
-                        return state, len(priority)
-                        
-    return state, len(priority)
-
-
-puzzle = []
-
-puzzle.append(2)
-puzzle.append(8)
-puzzle.append(3)
-puzzle.append(1)
-puzzle.append(6)
-puzzle.append(4)
-puzzle.append(7)
-puzzle.append(0)
-puzzle.append(5)
-
-goal = []
-
-goal.append(1)
-goal.append(2)
-goal.append(3)
-goal.append(8)
-goal.append(0)
-goal.append(4)
-goal.append(7)
-goal.append(6)
-goal.append(5) 
-
-
-state, visited = evaluvate_misplaced(puzzle, goal) 
-bestpath = bestsolution(state)
-print(str(bestpath).replace('[', ' ').replace(']', ''))
-totalmoves = len(bestpath) - 1
-print('\nSteps to reach goal:',totalmoves)
-visit = len(state) - visited
-print('Total nodes visited: ',visit, "\n")
+a_star(10, 20, 100)

98c22377ccc6346a4af88e33e05738a99d96f5f3
Gurucharan Shivakumar <gurushivkrish@gmail.com>
2023-02-13 11:21:20 +0530

    added 'CSP'

---
+++ Py Programs/CSP/main.py
@@ -0,0 +1,61 @@
+class UniversalDict:
+    def __init__(self, value): self.value = value
+
+    def __getitem__(self, key): return self.value
+
+    def __repr__(self): return '{{Any: {0!r}}}'.format(self.value)
+
+
+def different_values_constraint(A, a, B, b):
+    """A constraint saying two neighboring variables must differ in value."""
+    return a != b
+
+
+def MapColoringCSP(colors, neighbors):
+    """Make a CSP for the problem of coloring a map with different colors
+    for any two adjacent regions. Arguments are a list of colors, and a
+    dict of {region: [neighbor,...]} entries. This dict may also be
+    specified as a string of the form defined by parse_neighbors."""
+    if isinstance(neighbors, str):
+        neighbors = parse_neighbors(neighbors)
+    return CSP(list(neighbors.keys()), UniversalDict(colors), neighbors, different_values_constraint)
+
+
+def parse_neighbors(neighbors):
+    """Convert a string of the form 'X: Y Z; Y: Z' into a dict mapping
+    regions to neighbors. The syntax is a region name followed by a ':'
+    followed by zero or more region names, followed by ';', repeated for
+    each region name. If you say 'X: Y' you don't need 'Y: X'.
+    >>> parse_neighbors('X: Y Z; Y: Z') == {'Y': ['X', 'Z'], 'X': ['Y', 'Z'], 'Z': ['X', 'Y']}
+    True
+    """
+    dic = defaultdict(list)
+    specs = [spec.split(':') for spec in neighbors.split(';')]
+    for (A, Aneighbors) in specs:
+        A = A.strip()
+        for B in Aneighbors.split():
+            dic[A].append(B)
+            dic[B].append(A)
+    return dic
+
+
+australia_csp = MapColoringCSP(list('RGB'), """SA: WA NT Q NSW V; NT: WA Q; NSW: Q V; T: """)
+
+usa_csp = MapColoringCSP(list('RGBY'),
+                         """WA: OR ID; OR: ID NV CA; CA: NV AZ; NV: ID UT AZ; ID: MT WY UT;
+                         UT: WY CO AZ; MT: ND SD WY; WY: SD NE CO; CO: NE KA OK NM; NM: OK TX AZ;
+                         ND: MN SD; SD: MN IA NE; NE: IA MO KA; KA: MO OK; OK: MO AR TX;
+                         TX: AR LA; MN: WI IA; IA: WI IL MO; MO: IL KY TN AR; AR: MS TN LA;
+                         LA: MS; WI: MI IL; IL: IN KY; IN: OH KY; MS: TN AL; AL: TN GA FL;
+                         MI: OH IN; OH: PA WV KY; KY: WV VA TN; TN: VA NC GA; GA: NC SC FL;
+                         PA: NY NJ DE MD WV; WV: MD VA; VA: MD DC NC; NC: SC; NY: VT MA CT NJ;
+                         NJ: DE; DE: MD; MD: DC; VT: NH MA; MA: NH RI CT; CT: RI; ME: NH;
+                         HI: ; AK: """)
+
+france_csp = MapColoringCSP(list('RGBY'),
+                            """AL: LO FC; AQ: MP LI PC; AU: LI CE BO RA LR MP; BO: CE IF CA FC RA
+                            AU; BR: NB PL; CA: IF PI LO FC BO; CE: PL NB NH IF BO AU LI PC; FC: BO
+                            CA LO AL RA; IF: NH PI CA BO CE; LI: PC CE AU MP AQ; LO: CA AL FC; LR:
+                            MP AU RA PA; MP: AQ LI AU LR; NB: NH CE PL BR; NH: PI IF CE NB; NO:
+                            PI; PA: LR RA; PC: PL CE LI AQ; PI: NH NO CA IF; PL: BR NB CE PC; RA:
+                            AU BO FC PA LR""")
